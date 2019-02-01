4b71bb5f71d932cbc93b0abcd757b4f3d2e9157f
catslanderer <45492869+catslanderer@users.noreply.github.com>
2019-01-31 23:33:52 -0500

    MVP-review: I started with empty copies of Dropdown.js and Tabs.js to see if I could rebuild them without the guiding comments (still using MDN, etc.). Dropdown.js complete!

--- components/Dropdown/Dropdown.js
+++ components/Dropdown/Dropdown.js
@@ -1,33 +1,21 @@
-class Dropdown {
+class Menu {
   constructor(element) {
-    // Assign this.element to the dropdown element
-    this.element = element;
-
-    // Get the element with the ".dropdown-button" class found in the dropdown element (look at the HTML for context)
-    this.button = this.element.querySelector(".dropdown-button");
-
-    // assign the reference to the ".dropdown-content" class found in the dropdown element
-    this.content = this.element.querySelector(".dropdown-content");
-    this.show = false; // GSAP
-
-    // Add a click handler to the button reference and call the toggleContent method.
-    this.button.addEventListener("click", () => this.toggleContent());
+    this.Menu = element;
+    this.menuButton = this.Menu.querySelector(".dropdown-button");
+    this.menuContent = this.Menu.querySelector(".dropdown-content");
+    this.menuOpen = false;
+    this.menuButton.addEventListener("click", () => this.revealMenu());
   }
-
-  toggleContent() {
-    // GSAP
-    if (this.show === false) {
-      this.show = true;
-      TweenMax.to(this.content, 0, { display: "flex" });
-      TweenMax.to(this.content, 2, { opacity: 1 });
+  revealMenu() {
+    if (this.menuOpen === false) {
+      this.menuOpen = true;
+      TweenMax.to(this.menuContent, 0, { display: "flex" });
+      TweenMax.to(this.menuContent, 2, { opacity: 1 });
     } else {
-      this.show = false;
-      TweenMax.to(this.content, 0.5, { opacity: 0, display: "none" });
+      this.menuOpen = false;
+      TweenMax.to(this.menuContent, 0.5, { opacity: 0, display: "none" });
     }
   }
 }
 
-// Nothing to do here, just study what the code is doing and move on to the Dropdown class
-let dropdowns = document
-  .querySelectorAll(".dropdown")
-  .forEach(dropdown => new Dropdown(dropdown));
+const navMenu = new Menu(document.querySelector(".dropdown"));
